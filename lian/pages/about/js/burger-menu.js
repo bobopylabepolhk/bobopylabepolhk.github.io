7a46ddbdc847ccae9cee4de21993c4001dd112b9
atyursrvice <atyursrvice@gmail.com>
2017-12-04 20:21:31 +0500

    Adding a new React app

--- lian/pages/about/js/burger-menu.js
+++
@@ -1,7 +0,0 @@
-var menu = document.querySelector('.header-nav__burger-menu');
-var list = document.querySelector('.header-nav-list')
-
-menu.addEventListener('click', function(){
-    this.classList.toggle('open');
-    list.classList.toggle('unfolded');
-}, true);
