5bb912defa97e5b309cb66d9ffd9c5b42b411dc0
atyursrvice <atyursrvice@gmail.com>
2017-12-14 21:56:17 +0500

    Updates

--- barber/js/burger-menu.js
+++
@@ -1,7 +0,0 @@
-var menu = document.getElementById('burgerMenuIcon');
-var menuList = document.getElementById('burgerMenuList');
-
-menu.addEventListener('click', function() {
-    this.classList.toggle('open');
-    menuList.classList.toggle('unfolded');
-});
