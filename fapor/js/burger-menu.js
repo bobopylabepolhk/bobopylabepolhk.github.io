d3421e7ec5b5cdbdc1cdde953066e244ba65a5c2
atyursrvice <atyursrvice@gmail.com>
2018-09-09 23:59:57 +0500

    Deleted unnecessary projects, updated peregovorki

--- fapor/js/burger-menu.js
+++
@@ -1,7 +0,0 @@
-var icon = document.getElementById('menuIcon');
-var menu = document.getElementById('menu');
-
-icon.addEventListener('click', function(){
-    this.classList.toggle('open');
-    menu.classList.toggle('_unfolded');
-});
