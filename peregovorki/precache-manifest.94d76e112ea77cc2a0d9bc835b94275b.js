6be35adda30605a5d555fdb6ebb3dd23ed0ffaae
atyursrvice <atyursrvice@gmail.com>
2018-10-22 18:29:13 +0500

    Users now can't take part in multiple events at the same time

---
+++ peregovorki/precache-manifest.94d76e112ea77cc2a0d9bc835b94275b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "eebc7d280c57d67a6b92",
+    "url": "/peregovorki/static/js/5.eebc7d28.chunk.js"
+  },
+  {
+    "revision": "239f0e345cc78df9a804",
+    "url": "/peregovorki/static/js/0.239f0e34.chunk.js"
+  },
+  {
+    "revision": "1ccfd146e9f988db95e5",
+    "url": "/peregovorki/static/js/main.1ccfd146.chunk.js"
+  },
+  {
+    "revision": "9f4146cb33028991668d2f4ec77a5fab",
+    "url": "/peregovorki/static/media/Arimo.9f4146cb.woff"
+  },
+  {
+    "revision": "ef5dbedffe29a82de2df",
+    "url": "/peregovorki/static/js/2.ef5dbedf.chunk.js"
+  },
+  {
+    "revision": "26a0caab182e455e437af04ce0159180",
+    "url": "/peregovorki/static/media/Arimo-Bold.26a0caab.woff"
+  },
+  {
+    "revision": "c52298680001cee60330",
+    "url": "/peregovorki/static/js/3.c5229868.chunk.js"
+  },
+  {
+    "revision": "5379da4d14cbe2bbee84a07fbb6ebc97",
+    "url": "/peregovorki/static/media/TextBookBold.5379da4d.woff"
+  },
+  {
+    "revision": "e3e89d67d6cfb3c48473",
+    "url": "/peregovorki/static/js/4.e3e89d67.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/peregovorki/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6a7c32fdbf4e8d716f3b922bfd576471",
+    "url": "/peregovorki/static/media/TextbookNormal.6a7c32fd.woff"
+  },
+  {
+    "revision": "83d5cecc5173df144d71",
+    "url": "/peregovorki/static/js/6.83d5cecc.chunk.js"
+  },
+  {
+    "revision": "75cb3d3975d5a8d3e66d",
+    "url": "/peregovorki/static/js/7.75cb3d39.chunk.js"
+  },
+  {
+    "revision": "0405766c91bd53bb84eb",
+    "url": "/peregovorki/static/js/runtime~main.0405766c.js"
+  },
+  {
+    "revision": "f7a50b39d52d79c5a2628f5d30b08837",
+    "url": "/peregovorki/static/media/logo.f7a50b39.png"
+  },
+  {
+    "revision": "1ccfd146e9f988db95e5",
+    "url": "/peregovorki/static/css/main.1167c1d2.chunk.css"
+  },
+  {
+    "revision": "eebc7d280c57d67a6b92",
+    "url": "/peregovorki/static/css/5.f295511c.chunk.css"
+  },
+  {
+    "revision": "e3e89d67d6cfb3c48473",
+    "url": "/peregovorki/static/css/4.481df0aa.chunk.css"
+  },
+  {
+    "revision": "c52298680001cee60330",
+    "url": "/peregovorki/static/css/3.7b332f04.chunk.css"
+  },
+  {
+    "revision": "ef5dbedffe29a82de2df",
+    "url": "/peregovorki/static/css/2.d9fd2a26.chunk.css"
+  },
+  {
+    "revision": "eb95dea7cc0e5e0a1f77a4ebcf1fae81",
+    "url": "/peregovorki/index.html"
+  }
+];
